0b7b33769def48bd97bef92940c12b3954c6c318
Javier Marín Nano <nano.world.contact@gmail.com>
2015-06-12 23:42:10 -0430

    deploy from ship

--- README.md
+++
@@ -1,1 +0,0 @@
-nanotime/nanotime.github.io
---
+++ js/main.js
@@ -0,0 +1,10 @@
+(function() {
+  $('body').nanoPaginator({
+    posts: 3,
+    postsContainer: '.content',
+    postItem: '.post-item'
+  });
+
+}).call(this);
+
+ //# sourceMappingURL=main.js.map
